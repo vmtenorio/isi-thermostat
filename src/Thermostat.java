053af70b60c723704cc555fd1733b7ba678aa9b9
Victor Manuel Tenorio <bossipo@debian-Bossipo.portatil-Bossipo.com>
2017-12-27 11:24:17 +0100

    Añadido constructor y atributos

--- src/Thermostat.java
+++ src/Thermostat.java
@@ -1,4 +1,20 @@
 
 public class Thermostat {
+	
+	public enum PartOfDay {
+		Sleep, Wake;
+	}
+	
+	public enum Temp {
+		High, Low;
+	}
+	
+	private PartOfDay partOfDay;
+	private Temp temp;
+	
+	public Thermostat() {
+		partOfDay = PartOfDay.Wake;
+		temp = Temp.Low;
+	}
 
 }
