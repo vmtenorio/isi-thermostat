ce083672b9f4f6537a34a260f6c2ba1d8ba89676
Victor Manuel Tenorio <bossipo@debian-Bossipo.portatil-Bossipo.com>
2017-12-27 11:12:18 +0100

    Codigo del test 1

--- src/ThermostatTest.java
+++ src/ThermostatTest.java
@@ -1,22 +1,28 @@
 import static org.junit.Assert.*;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class ThermostatTest {
 
+	Thermostat t;
+	
 	@Before
 	public void setUp() throws Exception {
+		t = new Thermostat();
 	}
 
-	@After
-	public void tearDown() throws Exception {
-	}
-
+	// Test para e0 -> e3
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test1() {
+		t.up();
+		assertTrue("Falla e0", t.getPartOfDay() == Wake && t.getTemp() == High);
+		t.advance();
+		assertTrue("Falla e1", t.getPartOfDay() == Sleep && t.getTemp() == High);
+		t.down();
+		assertTrue("Falla e2", t.getPartOfDay() == Sleep && t.getTemp() == Low);
+		t.advance();
+		assertTrue("Falla e3", t.getPartOfDay() == Wake && t.getTemp() == High);
 	}
 
 }
